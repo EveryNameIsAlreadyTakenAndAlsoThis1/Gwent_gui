e0c80af4da51ea5f5dbccfc9ba5f6d7c24e9639f
L00PER <aandrejkuzmarin@gmail.com>
2023-05-18 02:07:59 +0200

    fullscreen scaled cards

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
 
+def scale_surface(surface, target_size):
+    """Scales a Pygame surface proportionally to fit into the target size."""
+    target_width, target_height = target_size
+    width, height = surface.get_size()
+    scale_factor = min(target_width / width, target_height / height)
+    new_size = (round(width * scale_factor), round(height * scale_factor))
+    return pygame.transform.smoothscale(surface, new_size)
+
+
 class DraggableImage:
-    def __init__(self, x, y, image_path, image_size=(50, 50)):
-        self.image = pygame.image.load(image_path)
-        self.image = pygame.transform.scale(self.image, image_size)  # Resize the image
-        self.hover_image = pygame.transform.scale(self.image, (75, 75))  # An enlarged version of the image
+    def __init__(self, x, y, image_path, game_width, game_height):
+        original_image = pygame.image.load(image_path)
+        self.image_size = (game_width // 10, game_height // 7)  # Updated dimensions
+        self.hover_image_size = (game_width // 5, game_height // 3.5)  # Updated dimensions
+        self.image = scale_surface(original_image, self.image_size)
+        self.hover_image = scale_surface(original_image, self.hover_image_size)
         self.rect = self.image.get_rect(topleft=(x, y))
         self.dragging = False
         self.hovering = False
-        self.selected = False  # new attribute
+        self.selected = False
+
 
     def draw(self, screen):
-        if self.hovering and not self.dragging:  # Draw the enlarged image if the mouse is hovering over the image and the image is not being dragged
-            screen.blit(self.hover_image, self.rect)
+        if self.hovering and not self.dragging:
+            screen.blit(self.hover_image, (self.rect.x - (self.hover_image_size[0] - self.image_size[0]) // 2,
+                                           self.rect.y - (self.hover_image_size[1] - self.image_size[1]) // 2))
         else:
             screen.blit(self.image, self.rect)
 
@@ -20,41 +33,45 @@ class DraggableImage:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
                 self.dragging = True
+                self.hovering = False  # Update here to stop hovering effect when dragging
                 mouse_x, mouse_y = event.pos
                 self.offset_x = self.rect.x - mouse_x
-                self.offset_y = self.rect.y - mouse_y
-                return self  # Return the image itself when it starts being dragged
+                self.offset_y = self.rect.y - mouse_y  # Fixed typo here
+                return self
+
         elif event.type == pygame.MOUSEBUTTONUP:
             self.dragging = False
             if not (green_rect.colliderect(self.rect) or red_rect.colliderect(self.rect)):
-                self.rect.topleft = green_rect.topleft  # Snap back to green rectangle if outside both rectangles
+                self.rect.topleft = green_rect.topleft
         elif event.type == pygame.MOUSEMOTION:
             if self.dragging:
                 mouse_x, mouse_y = event.pos
                 self.rect.x = mouse_x + self.offset_x
                 self.rect.y = mouse_y + self.offset_y
-                return self  # Also return the image itself when it's being dragged
+                return self
+
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:  # 3 is for right button
             if self.rect.collidepoint(event.pos):
                 self.selected = not self.selected
                 return self
 
-    def update(self, mouse_pos):
-        if not self.dragging:  # Only check for hover when the image is not being dragged
+    def update(self, mouse_pos, dragged_image):
+        if not self.dragging and self != dragged_image:
             if self.rect.collidepoint(mouse_pos):
                 self.hovering = True
             else:
                 self.hovering = False
 
-
 class GameObject:
     def __init__(self, screen_width, screen_height):
         self.screen_width = screen_width
         self.screen_height = screen_height
-        self.green_rect = pygame.Rect(100, 100, 200, 200)  # Define the green rectangle
-        self.red_rect = pygame.Rect(500, 100, 200, 200)  # Define the red rectangle
+        self.image_width = self.screen_width // 10
+        self.image_height = self.screen_height // 7
+        self.green_rect = pygame.Rect(0, self.screen_height - (self.image_height * 2), self.image_width * 10, self.image_height * 2)
+        self.red_rect = pygame.Rect(self.screen_width // 2 - (self.image_width * 5), self.screen_height // 2 - self.image_height, self.image_width * 10, self.image_height * 2)
         self.images = [
-            DraggableImage(self.green_rect.x + 50 * (i % 4), self.green_rect.y + 50 * (i // 4), f'decoy.png')
+            DraggableImage(self.green_rect.x + self.image_width * (i % 10), self.green_rect.y + self.image_height * (i // 10), 'decoy.png', self.screen_width, self.screen_height)
             for i in range(8)  # Change the range to the number of images you have
         ]
         self.dragged_image = None
@@ -97,23 +114,26 @@ class GameObject:
         red_images = [img for img in self.images if self.red_rect.colliderect(img.rect)]
 
         for i, img in enumerate(green_images):
-            img.rect.topleft = (self.green_rect.x + 50 * (i % 4), self.green_rect.y + 50 * (i // 4))
+            img.rect.topleft = (self.green_rect.x + self.image_width * i, self.green_rect.y)
         for i, img in enumerate(red_images):
-            img.rect.topleft = (self.red_rect.x + 50 * (i % 4), self.red_rect.y + 50 * (i // 4))
+            img.rect.topleft = (self.red_rect.x + self.image_width * i, self.red_rect.y)
+
+    # ... Rest of the class
 
     def update(self):
         mouse_pos = pygame.mouse.get_pos()
         for image in self.images:
-            image.update(mouse_pos)
+            image.update(mouse_pos, self.dragged_image)
 
 
 class Game:
-    def __init__(self, width=800, height=600, fps=60):
+    def __init__(self, fps=60):
         pygame.init()
-        self.width = width
-        self.height = height
+        infoObject = pygame.display.Info()
+        self.width = infoObject.current_w
+        self.height = infoObject.current_h
         self.fps = fps
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.screen = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN)
         self.clock = pygame.time.Clock()
         self.running = True
 
@@ -129,6 +149,9 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
             self.game_object.handle_event(event)
 
     def update(self):
@@ -145,6 +168,16 @@ class MyGame(Game):
     def __init__(self):
         super().__init__()
         self.game_object = GameObject(self.width, self.height)  # pass the width and height
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+            self.game_object.handle_event(event)
+
 
 if __name__ == '__main__':
     game = MyGame()
